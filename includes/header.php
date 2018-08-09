cca6c5d2052d5de8b9701a8bf595e787d6b83c7a
shivam-kapila <shivamkapila4@gmail.com>
2018-08-09 17:35:52 +0530

    Initial commit.

---
+++ includes/header.php
@@ -0,0 +1,332 @@
+<div class="navbar navbar-inverse navbar-fixed-top"> 
+    <div class="container"> 
+        <div class="navbar-header"> 
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> 
+                <span class="icon-bar"></span> 
+                <span class="icon-bar"></span> 
+                <span class="icon-bar"></span>
+            </button> 
+                <a class="navbar-brand" href="#">
+                    <b><i class="fas fa-folder-open"></i> Curriculum Vitae </b>
+                </a> 
+        </div> 
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right"> 
+                <li><a href="#info1" data-toggle="modal"><i class="fas fa-user"></i> Personal Info</a</li> 
+                <li><a href="#skills1" data-toggle="modal"><i class="fas fa-tags"></i> Skills</a></li> 
+                <li><a href="#interests1" data-toggle="modal"><i class="fas fa-boxes"></i> Interests</a></li>  
+            </ul>
+        </div> 
+    </div> 
+</div>
+
+<div class="modal" id="info1">
+
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>PERSONAL INFO
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <h3 class="trial"><b>NAME </b> <span class="name">Shivam Kapila</span></h3> 
+                      <h5><b>BRANCH </b> Computer Science & Technology [Dual Degree]</h5>
+                        <h5><b>ROLL NO. </b> 17MI534</h5>
+                        <br>
+                        <div>  <a href="#info2" data-toggle="modal" class="button2 btn btn-primary active" role="button" class="close" data-dismiss="modal"> Next</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+  </div>
+
+<div class="modal" id="info2">
+
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>CONTACT INFO
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+                      <h5><b>PHONE NO.</b> 97360-66176</h5>
+                        <h5><b>EMAIL ID </b> shivamkapila4@gmail.com</h5>
+                        <br>
+                        <div>  <a href="#info2" data-toggle="modal" class="button2 btn btn-primary active" role="button"> Exit</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+<div class="modal" id="skills1">
+
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>WEB DEVELOPMENT
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>HTML</b></h5></li>
+              <li><h5><b>CSS</b></h5></li>
+              <li><h5><b>Bootstrap</b></h5></li>
+              <li><h5><b>PHP</b></h5></li>
+              <li><h5><b>Javascript </b>[Just Basics]</h5></li>
+          </ul>
+                        <br>
+                        <div>  <a href="#skills2" data-toggle="modal" class="button2 btn btn-primary active" role="button" class="close" data-dismiss="modal"> Next</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+<div class="modal" id="skills2">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>EDITING
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>Adobe Photoshop</b> [Just Basics]</h5></li>
+
+          </ul>
+                        <br>
+                        <div>  <a href="#skills3" data-toggle="modal" class="button2 btn btn-primary active" role="button" class="close" data-dismiss="modal"> Next</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+
+
+<div class="modal" id="skills3">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>BACK END DEVELOPMENT
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>MySQL</b> </h5></li>
+
+          </ul>
+                        <br>
+                        <div>  <a href="#skills4" data-toggle="modal" class="button2 btn btn-primary active" role="button" class="close" data-dismiss="modal"> Next</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+
+<div class="modal" id="skills4">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>ANDROID DEVELOPMENT
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>Kotlin</b> </h5></li>
+
+          </ul>
+                        <br>
+                        <div>  <a href="#skills4" data-toggle="modal" class="button2 btn btn-primary active" role="button"> Exit</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+<div class="modal" id="interests1">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>PROJECT DEVELOPMENT
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>Web Development</b></h5></li>
+              <li><h5><b>Android Development</b></h5></li>
+          </ul>
+                        <br>
+                         <div>  <a href="#interests2" data-toggle="modal" class="button2 btn btn-primary active" role="button" class="close" data-dismiss="modal"> Next</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+
+<div class="modal" id="interests2">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>JUST FOR FUN
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>Watching TV Series</b></h5></li>
+              <li><h5><b>Enjoying Movies</b> [Novel Based]</h5></li>
+          </ul>
+                        <br>
+                        <div>  <a href="#interests2" data-toggle="modal" class="button2 btn btn-primary active" role="button"> Exit</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+<div class="modal" id="projects1">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>WEB BASED
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>Lifestyle Store</b> [E-Commerce Website]</h5></li>
+              <li><h5><b>E-Store</b> [Online Smart Phone Shop]</h5></li>
+              <li><h5><b>PhotoGrid</b> [A Basic Picture Gallery]</h5></li>
+          </ul>
+                        <br>
+                          <div>  <a href="#projects2" data-toggle="modal" class="button2 btn btn-primary active" role="button" class="close" data-dismiss="modal"> Next</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+
+<div class="modal" id="projects2">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>ANDROID BASED
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>My Profile</b> [A Basic Profile App]</h5></li>
+              <li><h5><b>Echo Echo</b> [Music Player With Shake To Change]</h5></li>
+          </ul>
+                        <br>
+                        <div>  <a href="#projects2" data-toggle="modal" class="button2 btn btn-primary active" role="button"> Exit</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+<div class="modal" id="clubs1">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>ANDROID BASED
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5><b>CSEC</b></h5></li>
+          </ul>
+                        <br>
+                        <div>  <a href="#clubs1" data-toggle="modal" class="button2 btn btn-primary active" role="button"> Exit</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
+
+
+<div class="modal" id="reason1">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+          <div class="head"><h4>WORK ON REAL WORLD PROJECTS
+                  <button type="button" class="close" data-dismiss="modal">&times;</button><h4>
+          </div></div>
+
+      <div class="modal-body">
+          <ul type = "square">
+              <li><h5>Being a part of <b>Pixonoids Web Team</b> I will have opportunities to work on read world web
+                  based projects which will help me to nurture my skills and learn new things as well. 
+                  I will also have a chance to explore Photography, After Effects, Illustrator and so on as well as improve my hand at Photoshop too.</h5></li>
+              </ul>
+                        <br>
+                        <div>  <a href="#reason1" data-toggle="modal" class="button2 btn btn-primary active" role="button"> Exit</a>
+        </div>
+                    
+      </div>
+      </div>
+
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+        <!--jQuery library--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Orbitron:900|Righteous" rel="stylesheet">    
+<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet"> 
+<!--Latest compiled and minified JavaScript--> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link href="css/index.css" rel="stylesheet" type="text/css"/> 
+        <title>Curriculum Vitae</title>
+    </head>
+    <body>
+        <?php
+        include 'includes/header.php';
+        ?>
+        <br>
+        <br>
+        <br>
+        
+        <div id="banner_image">
+            <div class="container-fluid">
+                <center> <div class="jumbotron" id = "content">
+                    <h1 class="head">
+                       <i class="fas fa-camera-retro"></i>
+                        CURRICULUM VITAE
+                    </h1>       
+                    </div>
+                    <h2><img src="images/1.png" height="35px" width="35px"> TEAM PIXONOIDS</h2>
+                    <br>
+                    <br>
+                    <br>
+        
+                    
+                    
+                    <div class ="row row-style">
+                            <div class = "col-sm-4">
+                                 <a href="#info1" data-toggle="modal" class="button btn btn-success btn-lg active" role="button"><i class="fas fa-user"></i> Personal Info</a>
+                            </div>
+                            <div class = "col-sm-4">
+                           <a href="#skills1" data-toggle="modal" class="button btn btn-success btn-lg active" role="button"><i class="fas fa-tags"></i> Skills</a>
+
+                            </div>
+                            <div class = "col-sm-4">
+                             <a href="#reason1" data-toggle="modal" class="button btn btn-success btn-lg active" role="button"><i class="fas fa-boxes"></i> Interests</a>
+
+                            </div>
+                    </div>
+                    <br>
+                    <br>
+                     <div class ="row row-style">
+                            <div class = "col-sm-4">
+                                 <a href="#projects1" data-toggle="modal" class="button3 btn btn-primary btn-lg active" role="button"><i class="fas fa-project-diagram"></i> Projects</a>
+                            </div>
+                            <div class = "col-sm-4">
+                           <a href="#clubs1" data-toggle="modal" class="button3 btn btn-primary btn-lg active" role="button"><i class="fas fa-users"></i> Other Clubs/Societies</a>
+
+                            </div>
+                            <div class = "col-sm-4">
+                             <a href="#reason1" data-toggle="modal" class="button3 btn btn-primary btn-lg active" role="button"><i class="fas fa-grin-beam-sweat"></i> Reason For Joining</a>
+
+                            </div>
+                    </div>
+                </center>
+            </div>
+                         
+                    </body>
+                        </html>
